dad012e7a9e8f45e3f44f6ae2154043f1122b13a
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-21 19:15:55 -0400

    adding navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './Navbar.css'
+import Logo from '../assets/img/logo.png'
 
 export default function Navbar() {
     return (
-        <div>
-            
+        <div className='navbar'>
+            <h1 className='navbar-title'>Limp!</h1>
+            <img 
+                src={Logo} 
+                alt='logo' 
+                className='navbar-logo'
+            />
         </div>
     )
 }
