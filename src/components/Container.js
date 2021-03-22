aa0ac0fa6d3c52522f9971eee1cda4dc9538e1b8
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-22 19:45:34 -0400

    adding container

---
+++ src/components/Container.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Container.css'
+
+export default function Container() {
+    return (
+        <section className='section-container'>
+            Main Container
+        </section>
+    )
+}
