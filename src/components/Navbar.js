78b103a909546fa42b79b3e535a49c9f98062c07
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-21 20:28:01 -0400

    adding semantic html

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import Logo from '../assets/img/logo.png'
 
 export default function Navbar() {
     return (
-        <div className='navbar'>
+        <nav className='navbar'>
             <h1 className='navbar-title'>Limp!</h1>
             <img 
                 src={Logo} 
                 alt='logo' 
                 className='navbar-logo'
             />
-        </div>
+        </nav>
     )
 }
