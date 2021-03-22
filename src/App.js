fa6a4f06b6203bd7a45c10cb1a082478e6aad346
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-21 20:29:09 -0400

    testing dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Navbar from './components/Navbar'
+import Drawer from './components/Drawer'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Drawer />
+      <Footer />
+    </>
   );
 }
 
