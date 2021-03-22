3b1bb152cafe43265e6ebfc0028e9b247cccd777
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-22 19:24:09 -0400

    adding loader app

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Loader.css'
+
+export default function Loader() {
+    return (
+        <div className='container-loader'>
+            <div class="lds-ripple">
+                <div></div>
+                <div></div>
+            </div>
+            <h3 className='loader-info'>Cargando ..</h3>
+        </div>
+    )
+}
