8469ed58788e9d4365b26d3069ec08a02b89c6bf
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-22 18:53:23 -0400

    adding navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import Logo from '../assets/img/logo.png'
 
 export default function Navbar() {
+    const [toggle, setToggle] = useState(true)
+
+    const open = () => {
+        setToggle(!toggle)
+    }
+
     return (
         <nav className='navbar'>
-            <h1 className='navbar-title'>Limp!</h1>
-            <img 
-                src={Logo} 
-                alt='logo' 
-                className='navbar-logo'
-            />
+            <div className='navbar-toggle'>
+                {
+                    toggle ? <p onClick={open} className='toggle-box'>=</p> 
+                           : <p onClick={open} className='toggle-box'>X</p>
+                }
+            </div>
+            <div className='navbar-info'>
+                <h1 className='navbar-title'>Limp!</h1>
+                <img 
+                    src={Logo} 
+                    alt='logo' 
+                    className='navbar-logo'
+                />
+            </div>
         </nav>
     )
 }
