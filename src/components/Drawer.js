3073f1a63ae69fbc1b88536006c8ffa662ffe297
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-23 20:30:05 -0400

    rendering dynamic routes

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,32 +1,51 @@
 import React from 'react'
 import './Drawer.css'
 
-export default function Drawer() {
+import {Link} from "react-router-dom";
+
+export default function Drawer({nav}) {
     return (
-        <aside className='drawer-container'>
+        <nav className='drawer-container'>
             <div className='drawer-title'>
                 <h2>Hola, Jesus &#x1F44B;</h2>
             </div>
             <div className='drawer-separator'></div>
             <div className='drawer-options'>
                 <ul className='drawer-options-list'>
-                    <li className='drawer-options-list-item'>Opcion 1</li>
-                    <li className='drawer-options-list-item'>Opcion 2</li>
-                    <li className='drawer-options-list-item'>Opcion 3</li>
-                    <li className='drawer-options-list-item'>Opcion 4</li>
-                    <li className='drawer-options-list-item'>Opcion 5</li>
-                    <div className='drawer-options-list-item'>
-                    <li>Opcion 6</li>
-                    </div>
+                    {
+                        nav.map((route, index) => {
+                            return (
+                                    <li key={index}>
+                                        <Link
+                                            className='drawer-options-list-item'
+                                            to={route.path}
+                                        >
+                                            {route.icon} {route.name}
+                                        </Link>
+                                    </li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
             <div className='drawer-separator'></div>
             <div className='drawer-config'>
                 <ul className='drawer-options-list'>
-                    <li className='drawer-options-list-item'>Cambiar Contrasena</li>
-                    <li className='drawer-options-list-item'>Cerrar Sesion</li>
+                    <li>
+                        <Link
+                            className='drawer-options-list-item'
+                            to='/Cambiar'
+                        >
+                            *- Cambiar Contrasena
+                        </Link>
+                    </li>
+                    <li className='drawer-options-list-item' 
+                        onClick={() => alert('Adios')}
+                    >
+                        *- Cerrar Sesion
+                    </li>
                 </ul>
             </div>
-        </aside>
+        </nav>
     )
 }
