39228d5ef66a974ff7929e6fe2ef9cf28cd0c71c
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-23 20:31:31 -0400

    rendering routes (components)

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Container.css'
 
-export default function Container() {
+import ChangePassword from '../views/ChangePassword'
+import NotFound from '../views/NotFound'
+
+import { Switch, Route } from 'react-router-dom';
+
+export default function Container({routes}) {
     return (
         <section className='section-container'>
-            Main Container
+            <Switch>
+                {
+                    routes.map((route, index) => {
+                        return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    children={<route.main/>}
+                                />
+                            )
+                    })
+                }
+                <Route path='/cambiar' component={ChangePassword}/>
+                <Route path='*' component={NotFound}/>
+            </Switch>
         </section>
     )
 }
