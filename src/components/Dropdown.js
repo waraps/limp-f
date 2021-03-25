2e47f8571edcf30ebe0b53b95ebf6ccab5c2bfbf
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-24 20:22:08 -0400

    adding drawer dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+
+export default function Dropdown({icon, name, routes}) {
+    const [open, setOpen] = useState(false)
+
+    const openDropdown = () => {
+        setOpen(!open)
+    }
+    return (
+        <div
+            className={open ? 'drawer-options-list-item-active' : 'drawer-options-list-item'}
+        >   
+                <div
+                    className='drawer-options-list-item-children-title' 
+                    onClick={openDropdown}
+                >
+                    {icon} {name} {open ? <span>&#128314;</span> : <span>&#128315;</span>}
+                </div>
+                <ul>
+                    {
+                        open && routes.map((route, index) => {
+                            return (
+                                <li key={index}>
+                                    <Link
+                                        className='drawer-options-list-item-child'
+                                        to={route.path}
+                                    >
+                                        {route.name}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+        </div>
+    )
+}
