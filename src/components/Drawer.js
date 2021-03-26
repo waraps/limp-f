7d9353d7a81c1dcd7ec817541d43ae25d8420807
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-26 19:47:49 -0400

    testing logout request

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {useHistory} from 'react-router-dom'
 import {Link} from "react-router-dom";
 import './Drawer.css'
+
+import { userData } from "../redux/loginSlice";
+import { logoutFetch } from '../redux/logoutSlice'
 
 // components
 import Dropdown from './Dropdown'
 
 export default function Drawer({nav}) {
+    const dispatch = useDispatch()
     const toggle = useSelector(state => state.toogle.isOpen)
+    const { user } = useSelector(userData)
     const history = useHistory();
 
     const logout = () => {
-        setTimeout(() => {
-            localStorage.clear()
-            history.replace('/login')
-        }, 1000);
+        dispatch(logoutFetch(history))
     }
     return (
         <nav className={toggle ? 'drawer-container-visible' : 'drawer-container'}>
             <div className='drawer-title'>
-                <h2>Hola, Jesus &#x1F44B;</h2>
+                {user ? <h2>Hola, {user.name} &#x1F44B;</h2> : <h2>Hola, bienvenido &#x1F44B;</h2>}
             </div>
             <div className='drawer-separator'></div>
             <div className='drawer-options'>
