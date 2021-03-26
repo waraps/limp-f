bad4beb59d769c71e712ed9e32bbe33c6973b2e6
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-26 19:48:25 -0400

    adding loader when user logout

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,8 +1,13 @@
+import { useSelector } from "react-redux";
+
 // Components
 import Navbar from '../components/Navbar'
 import Drawer from '../components/Drawer'
 import Container from '../components/Container'
 import Footer from '../components/Footer'
+import Loader from '../components/Loader'
+
+import { logoutStatus } from '../redux/logoutSlice'
 
 // Styles
 import '../assets/css/Dashboard.css'
@@ -11,6 +16,10 @@ import '../assets/css/Dashboard.css'
 import adminRoutes from '../routes/administrator'
 
 export default function Dashboard() {
+    const { loading, error } = useSelector(logoutStatus)
+
+    if(loading) return <Loader />
+
     return (
         <>
             <Navbar />
