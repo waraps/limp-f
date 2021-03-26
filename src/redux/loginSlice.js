34fe94a10be2d18d3e1a0c36194b9619fd01732f
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-26 19:45:44 -0400

    adding login redux slice

---
+++ src/redux/loginSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import auth from '../api/auth'
+
+export const loginSlice = createSlice({
+    name: 'login',
+    initialState: {
+        loading: false,
+        user: null,
+        error: false,
+    },
+    reducers: {
+        loginRequest: (state) => {
+            state.loading = true
+        },
+        loginSuccess: (state, action) => {
+            state.loading = false
+            state.user = action.payload
+        },
+        loginError: (state, action) => {
+            state.loading = false
+            state.error = action.payload
+        },
+    }
+})
+
+export const { loginRequest, loginSuccess, loginError } = loginSlice.actions
+
+export const userData = state => state.login
+
+export const loginFetch = (credentials, history) =>  async dispatch => {
+    try {
+        dispatch(loginRequest())
+        const response = await auth.signIn(credentials)
+        const {employee, access_token, refresh_token} = response.data
+        const user = JSON.stringify(employee)
+        localStorage.setItem('user', user)
+        localStorage.setItem('token', access_token)
+        localStorage.setItem('refresh', refresh_token)
+        dispatch(loginSuccess(employee))
+        history.replace('/')
+    } catch (error) {
+        dispatch(loginError(error))
+    }
+}
+
+export default loginSlice.reducer
