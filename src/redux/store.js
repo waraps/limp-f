bd3f4cbc2c36fd2c42fee955cd071eb3992dacf9
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-26 19:46:48 -0400

    adding login and logout reducers for the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import toogleReducer from './toogleSlice'
+import loginReducer from './loginSlice'
+import logoutReducer from './logoutSlice'
 
 export default configureStore({
     reducer: {
-        toogle: toogleReducer
+        toogle: toogleReducer,
+        login: loginReducer,
+        logout: logoutReducer,
     }
 })
