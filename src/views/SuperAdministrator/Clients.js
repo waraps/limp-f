96f9538c9a1a301f9ee10125c146afc118e86230
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-29 21:56:45 -0400

    adding client view

---
+++ src/views/SuperAdministrator/Clients.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from "react-redux";
+
+import { clientData, getClients } from '../../redux/slices/clientSlice';
+
+// components
+import Loader from '../../components/Loader'
+
+export default function Clients() {
+    const dispatch = useDispatch()
+    const { loading, error, clients } = useSelector(clientData)
+
+    useEffect(() => {
+        dispatch(getClients())
+    }, [])
+
+    return loading ? <Loader /> : (
+        <div>
+            {
+                clients ? 
+                            clients.map(client => {
+                                return <div key={client.id}>{client.name}</div>
+                            })
+                        :
+                        <div>No hay Clientes que mostrar</div>
+            }
+            {error && <div>{error}</div>}
+        </div>
+    )
+}
