ba5388bdbc06d731c1deee537fd459b350671292
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-29 21:49:30 -0400

    adding user select feature

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -7,16 +7,37 @@ import Container from '../components/Container'
 import Footer from '../components/Footer'
 import Loader from '../components/Loader'
 
-import { logoutStatus } from '../redux/logoutSlice'
+import { logoutStatus } from '../redux/slices/logoutSlice'
 
 // Styles
 import '../assets/css/Dashboard.css'
 
 // routes
-import adminRoutes from '../routes/administrator'
+import superAdminRoutes from '../router/routes/superAdministrator'
+import adminRoutes from '../router/routes/administrator'
 
 export default function Dashboard() {
-    const { loading, error } = useSelector(logoutStatus)
+    const { loading } = useSelector(logoutStatus)
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    let routes = null
+    switch (user.rol_id) {
+        case 1:
+            routes = superAdminRoutes
+            break;
+
+        case 2:
+            routes = adminRoutes
+            break;
+        
+        case 3:
+            routes = adminRoutes
+            break;
+    
+        default:
+            routes = null
+            break;
+    }
 
     if(loading) return <Loader />
 
@@ -24,8 +45,8 @@ export default function Dashboard() {
         <>
             <Navbar />
             <main className='main-container'>
-                <Drawer nav={adminRoutes}/>
-                <Container routes={adminRoutes}/>
+                <Drawer nav={routes}/>
+                <Container routes={routes}/>
             </main>
             <Footer />
         </>
