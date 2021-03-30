bf9027fca9ebf817474fe45507b3e76acdaebc49
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-29 21:58:07 -0400

    adding stores view

---
+++ src/views/SuperAdministrator/Stores.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from "react-redux";
+
+import { storeData, getStores } from '../../redux/slices/storeSlice';
+
+// components
+import Loader from '../../components/Loader'
+
+export default function Stores() {
+    const dispatch = useDispatch()
+    const { loading, error, stores } = useSelector(storeData)
+
+    useEffect(() => {
+        dispatch(getStores())
+    }, [])
+
+    return loading ? <Loader /> : (
+        <div>
+            {
+                stores ? 
+                            stores.map(store => {
+                                return <div key={store.id}>{store.name}</div>
+                            })
+                        :
+                        <div>No hay Tiendas que mostrar</div>
+            }
+            {error && <div>{error}</div>}
+        </div>
+    )
+}
