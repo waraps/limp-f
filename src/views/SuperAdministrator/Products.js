83111d35f638e4e8828e7fa127c7f930744df994
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-29 21:58:22 -0400

    adding products view

---
+++ src/views/SuperAdministrator/Products.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from "react-redux";
+
+import { productData, getProducts } from '../../redux/slices/productSlice';
+
+// components
+import Loader from '../../components/Loader'
+
+export default function Products() {
+    const dispatch = useDispatch()
+    const { loading, error, products } = useSelector(productData)
+
+    useEffect(() => {
+        dispatch(getProducts())
+    }, [])
+
+    return loading ? <Loader /> : (
+        <div>
+            {
+                products ? 
+                            products.map(product => {
+                                return <div key={product.id}>{product.name}</div>
+                            })
+                        :
+                        <div>No hay Products que mostrar</div>
+            }
+            {error && <div>{error}</div>}
+        </div>
+    )
+}
