41dc752aa2859b97339261860388d904ce12e1a8
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-03-29 21:56:03 -0400

    adding new slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import toogleReducer from './toogleSlice'
-import loginReducer from './loginSlice'
-import logoutReducer from './logoutSlice'
+import toogleReducer from './slices/toogleSlice'
+import loginReducer from './slices/loginSlice'
+import logoutReducer from './slices/logoutSlice'
+import storeReducer from './slices/storeSlice'
+import productReducer from './slices/productSlice'
+import clientReducer from './slices/clientSlice'
 
 export default configureStore({
     reducer: {
         toogle: toogleReducer,
         login: loginReducer,
         logout: logoutReducer,
+        store: storeReducer,
+        product: productReducer,
+        client: clientReducer,
     }
 })
